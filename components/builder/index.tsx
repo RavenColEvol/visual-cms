794c6a5892bd81e825b85e27d0afed38edf970f6
RavenColEvol <ravilamkoti@gmail.com>
2023-02-07 12:23:10 +0530

    add: sudo drag logic

--- components/builder/index.tsx
+++ components/builder/index.tsx
@@ -1,6 +1,29 @@
+import { DragEvent, useEffect, useRef } from "react";
+
 function Builder() {
   return (
-    <iframe src="/example" style={{width: '100%', height: '100%', outline: 'none', 'border': 'none'}}></iframe>
+    <div id='builder'>
+      <Dropzone />
+      <iframe src="/example" style={{width: '100%', height: '100%', outline: 'none', 'border': 'none'}}></iframe>
+    </div>
+  )
+}
+
+function Dropzone() {
+  const dropRef = useRef(null);
+
+  const handleDrop = (event: DragEvent) => {
+    event.preventDefault();
+    console.log('drop event', event);
+  }
+
+  const handleDragOver = (event: DragEvent) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }
+
+  return (
+    <div onDrop={handleDrop} onDragOver={handleDragOver} ref={dropRef} id="weeb-dropzone"></div>
   )
 }
 
--- components/visual_components/index.tsx
+++ components/visual_components/index.tsx
@@ -1,11 +1,70 @@
+import { LegacyRef, MutableRefObject, useEffect, useRef, useState } from "react";
+
+type dragCallback = {
+  item: object,
+}
+
+type dragResult = [
+  { isDragging: boolean },
+  LegacyRef<HTMLDivElement>
+]
+
+function useDrag(props:dragCallback): dragResult {
+  const dragRef = useRef(null);
+  const [state, setState] = useState({
+    ...props,
+    isDragging: false
+  });
+
+  useEffect(() => {
+    if(!dragRef.current) return;
+    const dom = dragRef.current as HTMLElement;
+    const builderDropzone = document.getElementById('weeb-dropzone') as HTMLDivElement;
+    console.log('builder', builderDropzone);
+    // drag
+    const handleDrag = (event: DragEvent) => {
+      builderDropzone.style.display = 'block';
+      setState(state => ({
+        ...state, 
+        isDragging: true
+      }))
+    }
+    const handleDragEnd = (event: DragEvent) => {
+      console.log('handle drag end', event);
+      builderDropzone.style.display = 'none';
+      setState(state => ({
+        ...state, 
+        isDragging: false
+      }))
+    }
+    dom.addEventListener('dragstart', handleDrag);
+    dom.addEventListener('dragend', handleDragEnd);
+
+    return () => {
+      dom.removeEventListener('dragstart', handleDrag);
+      dom.removeEventListener('dragend', handleDragEnd);
+    }
+  }, [dragRef])
+  
+  const { isDragging } = state;
+
+  return [{ isDragging }, dragRef];
+}
+
 function Text() {
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: 'text' }
+  });
+
   return (
     <div 
+    ref={dragRef}
     draggable={true}
     style={{ 
-        width: "100px", 
-        height: "100px", 
-        border: "1px solid black"
+        width: "50px", 
+        height: "50px", 
+        border: "1px solid black",
+        opacity: isDragging ? '.5' : '1'
     }}>
       Text
     </div>
